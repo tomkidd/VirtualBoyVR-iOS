9efe2a9b2f19ceb4db88863d04dc7d9f408c3b98
Tom Kidd <tomkidd@gmail.com>
2018-09-05 17:36:30 -0500

    Beginnings of cardboard view controller

---
+++ VirtualBoyVR-iOS/Provenance/Emulator/PVGCVRViewController.h
@@ -0,0 +1,14 @@
+//
+//  PVGCVRViewController.h
+//  VirtualBoyVR-iOS
+//
+//  Created by Tom Kidd on 9/3/18.
+//  Copyright © 2018 Tom Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GVRCardboardView.h"
+
+@interface PVGCVRViewController : UIViewController<GVRCardboardViewDelegate>
+
+@end
---
+++ VirtualBoyVR-iOS/Provenance/Emulator/PVGCVRViewController.m
@@ -0,0 +1,87 @@
+//
+//  PVGCVRViewController.m
+//  VirtualBoyVR-iOS
+//
+//  Created by Tom Kidd on 9/3/18.
+//  Copyright © 2018 Tom Kidd. All rights reserved.
+//
+
+#import "PVGCVRViewController.h"
+
+@interface PVGCVRViewController ()
+
+@end
+
+@implementation PVGCVRViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)cardboardView:(GVRCardboardView *)cardboardView drawEye:(GVREye)eye withHeadTransform:(GVRHeadTransform *)headTransform {
+    
+    // per-eye frame thing goes here
+    
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
+    
+}
+
+- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
+    
+}
+
+- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    
+}
+
+//- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize {
+//
+//}
+
+- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    
+}
+
+- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    
+}
+
+- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
+    
+}
+
+- (void)setNeedsFocusUpdate {
+    
+}
+
+//- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context {
+//
+//}
+
+- (void)updateFocusIfNeeded {
+    
+}
+
+@end
